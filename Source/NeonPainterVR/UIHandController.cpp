0439d8f8c44e69c397e12d3c9fbf3ea17bcde024
Jack Yang <jackyangzzh@gmail.com>
2021-02-15 17:38:49 -0600

    Button and other UIs in progress

--- Source/NeonPainterVR/UIHandController.cpp
+++ Source/NeonPainterVR/UIHandController.cpp
@@ -2,4 +2,20 @@
 
 
 #include "UIHandController.h"
+#include "InputCoreTypes.h"
 
+AUIHandController::AUIHandController()
+{
+	Pointer = CreateDefaultSubobject<UWidgetInteractionComponent>(TEXT("Pointer"));
+	Pointer->SetupAttachment(GetRootComponent());
+}
+
+void AUIHandController::TriggerPressed()
+{
+	Pointer->PressPointerKey(EKeys::LeftMouseButton);
+}
+
+void AUIHandController::TriggerReleased()
+{
+	Pointer->ReleasePointerKey(EKeys::LeftMouseButton);
+}
