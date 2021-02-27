05dd248f7a9720bb72483dc187498b0f09a6f4db
Jack Yang <jackyangzzh@gmail.com>
2021-02-27 15:41:18 -0600

    More progress on generating grid dynamically

---
+++ Source/NeonPainterVR/UI/Picker.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Picker.h"
+#include "Picker/PaintGrid.h"
+
+// Sets default values
+APicker::APicker()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+
+	PaintGrid = CreateDefaultSubobject<UWidgetComponent>(TEXT("PaintGrid"));
+	PaintGrid->SetupAttachment(GetRootComponent());
+
+	ActionBar = CreateDefaultSubobject<UWidgetComponent>(TEXT("ActionBar"));
+	ActionBar->SetupAttachment(GetRootComponent());
+}
+
+// Called when the game starts or when spawned
+void APicker::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UPaintGrid* PaintGridWidget = Cast<UPaintGrid>(PaintGrid->GetUserWidgetObject());
+
+	if (!PaintGridWidget) return;
+
+	PaintGridWidget->AddPainting();
+}
+
+// Called every frame
+void APicker::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
